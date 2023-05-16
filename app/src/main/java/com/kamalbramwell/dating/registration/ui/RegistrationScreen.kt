8d39d19a42e837009687d431c5e874d32ecc59ab
Kamal Bramwell <kdbramwell@gmail.com>
2023-05-16 17:14:32 -0400

    Created RegistrationScreen UI

--- app/src/main/java/com/kamalbramwell/dating/registration/ui/RegistrationScreen.kt
+++ app/src/main/java/com/kamalbramwell/dating/registration/ui/RegistrationScreen.kt
@@ -1,31 +1,87 @@
 package com.kamalbramwell.dating.registration.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kamalbramwell.dating.R
+import com.kamalbramwell.dating.registration.data.StockImageDataSource
+import com.kamalbramwell.dating.ui.components.MaxWidthBorderlessButton
+import com.kamalbramwell.dating.ui.components.MaxWidthButton
+import com.kamalbramwell.dating.ui.theme.DatingTheme
+import com.kamalbramwell.dating.utils.UiText
 
 @Composable
-fun RegistrationScreen() {
-    // Stock photos of people as bg
-    Column() {
+fun RegistrationScreen(
+    onEmailRegistrationClicked: () -> Unit = {},
+    onPhoneRegistrationClicked: () -> Unit = {},
+    onAlreadyRegisteredClicked: () -> Unit = {}
+) {
+    Box {
+        Image(
+            painter = painterResource(id = StockImageDataSource.random()),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
 
-        
-        EmailRegistrationButton()
-        PhoneRegistrationButton()
-        AlreadyRegisteredButton()
+        Column(
+            modifier = Modifier.fillMaxSize().padding(8.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            EmailRegistrationButton(onEmailRegistrationClicked)
+            PhoneRegistrationButton(onPhoneRegistrationClicked)
+            AlreadyRegisteredButton(onAlreadyRegisteredClicked)
+        }
     }
 }
 
 @Composable
-private fun EmailRegistrationButton() {
-
+private fun EmailRegistrationButton(onClick: () -> Unit = {}) {
+    MaxWidthButton(
+        label = UiText.StringResource(R.string.email_registration_cta),
+        onClick = onClick
+    )
 }
 
 @Composable
-private fun PhoneRegistrationButton() {
-
+private fun PhoneRegistrationButton(onClick: () -> Unit = {}) {
+    MaxWidthButton(
+        label = UiText.StringResource(R.string.phone_registration_cta),
+        onClick = onClick
+    )
 }
 
 @Composable
-private fun AlreadyRegisteredButton() {
+private fun AlreadyRegisteredButton(onClick: () -> Unit = {}) {
+    MaxWidthBorderlessButton(
+        label = UiText.StringResource(R.string.already_registered_cta),
+        onClick = onClick
+    )
+}
 
+@Preview(showBackground = true)
+@Composable
+private fun RegistrationScreenPreview() {
+    DatingTheme {
+        RegistrationScreen()
+    }
+}
+
+@Preview
+@Composable
+private fun RegistrationScreenDarkPreview() {
+    DatingTheme(darkTheme = true) {
+        RegistrationScreen()
+    }
 }
