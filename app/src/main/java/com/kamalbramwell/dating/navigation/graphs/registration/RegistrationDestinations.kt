54a0f9aaad7c18dfcfaa1177ac2dd4d17c836738
Kamal Bramwell <kdbramwell@gmail.com>
2023-05-29 13:41:48 -0400

    Fixed duplicated route error

--- app/src/main/java/com/kamalbramwell/dating/navigation/graphs/registration/RegistrationDestinations.kt
+++ app/src/main/java/com/kamalbramwell/dating/navigation/graphs/registration/RegistrationDestinations.kt
@@ -3,7 +3,7 @@ package com.kamalbramwell.dating.navigation.graphs.registration
 import com.kamalbramwell.dating.navigation.NavDestination
 
 enum class Registration : NavDestination {
-    RegistrationOptions { override val route = "registration" },
+    RegistrationOptions { override val route = "registration_options" },
     EmailRegistration { override val route = "email" },
     PhoneRegistration { override val route = "phone" },
     AccountLogin { override val route = "login" },
