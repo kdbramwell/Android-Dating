c5eff39c8d31e74c63cdb3bd8abe60b2b42231f7
Kamal Bramwell <kdbramwell@gmail.com>
2023-06-21 16:20:23 -0400

    Implemented OnboardingViewModel unit test

---
+++ app/src/test/java/com/kamalbramwell/dating/registration/ui/OnboardingViewModelTest.kt
@@ -0,0 +1,35 @@
+package com.kamalbramwell.dating.registration.ui
+
+import com.kamalbramwell.dating.registration.ui.onboarding.OnboardingViewModel
+import org.junit.Before
+import org.junit.Test
+
+class OnboardingViewModelTest {
+
+    private lateinit var viewModel: OnboardingViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = OnboardingViewModel()
+    }
+
+    @Test
+    fun `update state when response is received`() {
+
+    }
+
+    @Test
+    fun `update state when multiple choice option is clicked`() {
+
+    }
+
+    @Test
+    fun `allow navigation to next screen when required questions are answered`() {
+
+    }
+
+    @Test
+    fun `next is disabled if current question was not answered`() {
+
+    }
+}
