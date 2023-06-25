d0859aac1b11b284ab8677e5bcb04c08c672adc3
Kamal Bramwell <kdbramwell@gmail.com>
2023-06-25 08:14:55 -0400

    Added verticalScroll() modifier to OptionsRow

--- app/src/main/java/com/kamalbramwell/dating/onboarding/ui/components/QuestionItem.kt
+++ app/src/main/java/com/kamalbramwell/dating/onboarding/ui/components/QuestionItem.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.LocalTextStyle
@@ -130,6 +132,7 @@ private fun OptionsRow(
         modifier = Modifier
             .padding(16.dp)
             .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
     ) {
         options.forEach {
             MultipleChoiceOptionItem(option = it, onClick = onClick)
