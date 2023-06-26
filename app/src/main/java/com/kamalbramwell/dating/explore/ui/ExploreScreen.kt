79e67bf85c74245e65d5c69d2d7cab18cb1e15be
Kamal Bramwell <kdbramwell@gmail.com>
2023-06-26 13:27:34 -0400

    Updated CardStack size relative to screen space

--- app/src/main/java/com/kamalbramwell/dating/explore/ui/ExploreScreen.kt
+++ app/src/main/java/com/kamalbramwell/dating/explore/ui/ExploreScreen.kt
@@ -2,7 +2,9 @@ package com.kamalbramwell.dating.explore.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kamalbramwell.dating.explore.data.accounts
 import com.kamalbramwell.dating.explore.ui.components.CardStack
 import com.kamalbramwell.dating.ui.components.DatingText
@@ -28,13 +31,17 @@ fun ExploreScreen() {
         verticalArrangement = Arrangement.Center
     ) {
         if (!isEmpty) {
+            Spacer(Modifier.weight(2f))
             CardStack(
                 items = accounts,
-                onEmptyStack = { isEmpty = true }
+                onEmptyStack = { isEmpty = true },
+                modifier = Modifier.weight(6f).padding(horizontal = 16.dp)
             )
+            Spacer(Modifier.weight(1f))
         } else {
             DatingText(
                 text = UiText.DynamicString("No nearby users found."),
+                color = MaterialTheme.colorScheme.onPrimary,
                 style = MaterialTheme.typography.labelLarge
             )
         }
